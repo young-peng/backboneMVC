9e0cf730668449f100de6cc5a008c7c9641bea29
yao.peng <pengyao_2@163.com>
2015-12-28 17:56:29 +0800

    delete the unuse controller
    delete the unuse controller

--- js/controller/itemImportCtrl.js
+++
@@ -1,9 +0,0 @@
-define(['../view/itemimportView'], function (View) {
-
-    var controller = function () {
-        var view = new View();
-        view.render();
-        return view;
-    };
-    return controller;
-});
